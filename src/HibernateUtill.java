3cac88f0e478328f5af70cf5deea2241ad76bb85
Malinga Bandara <malinga1021@gmail.com>
2023-04-16 22:12:21 +0530

    Create hibernate.cfg.xml

--- src/HibernateUtill.java
+++ src/HibernateUtill.java
@@ -4,10 +4,7 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Environment;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class HibernateUtill {
 
@@ -17,19 +14,10 @@ public class HibernateUtill {
     static {
         try {
             if (sessionFactory == null) {
-                StandardServiceRegistryBuilder standardServiceRegistryBuilder
-                        = new StandardServiceRegistryBuilder();
-                Map<String, String> databaseConfiguration = new HashMap<>();
-                databaseConfiguration.put(Environment.URL,
-                        "jdbc:mysql://localhost:3306/asd_1?createDatabaseIfNotExist=true");
-                databaseConfiguration.put(Environment.USER, "root");
-                databaseConfiguration.put(Environment.PASS, "1234");
-                databaseConfiguration.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                databaseConfiguration.put(Environment.DIALECT, "org.hibernate.dialect.MySQL57Dialect");
-                standardServiceRegistryBuilder.applySettings(databaseConfiguration);
-
                 standardServiceRegistry
-                        = standardServiceRegistryBuilder.build();
+                        = new StandardServiceRegistryBuilder()
+                        .configure()
+                        .build();
                 MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
                 Metadata metadata = metadataSources.getMetadataBuilder().build();
                 sessionFactory = metadata.getSessionFactoryBuilder().build();
